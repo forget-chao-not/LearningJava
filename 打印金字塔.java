c6084e9351d5afb778bb97a71c9a4478e07ab6e8
forget-chao-not <1694751877@qq.com>
2018-03-12 16:53:32 +0800

    打印金字塔

---
+++ 打印金字塔.java
@@ -0,0 +1,38 @@
+/*
+	打印金字塔
+*/
+
+import java.io.*;
+
+public class Welcome 
+{
+	public static void main(String[] args) throws Exception
+	{
+		InputStreamReader isr=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(isr);
+			
+		System.out.println("请输入金字塔的层数：");
+		String ss=br.readLine();
+			
+		int layer=Integer.parseInt(ss);
+		
+		
+		for(int i=0;i<layer;i++)
+		{
+			//打印空格
+			for(int j=0;j<layer-i;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			//打印*号
+			for(int k=0;k<2*i+1;k++)
+			{
+				System.out.print("*");
+			}
+			
+			//打印换行
+			System.out.print("\n");
+		}
+	}
+}
